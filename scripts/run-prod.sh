2cec78d75efaa24d4bbaa459cb1405db5a249782
Dev Aggarwal <devxpy@gmail.com>
2023-03-09 19:58:36 +0530

    nicer run-prod script

--- scripts/run-prod.sh
+++ scripts/run-prod.sh
@@ -2,46 +2,19 @@
 
 set -x
 
-docker build . -t gooey-gpu
+NAME=gooey-gpu
 
-#NAME=deforum-sd
-#docker rm -f $NAME
-#docker run -d --restart always \
-#  --name $NAME \
-#  -v $PWD/checkpoints:/src/checkpoints \
-#  -v $HOME/.cache/huggingface:/root/.cache/huggingface \
-#  -v $HOME/.cache/torch:/root/.cache/torch \
-#  -e SENTRY_DSN='https://e388de9646144cd9ae13f96a631b1707@o425905.ingest.sentry.io/4504612832542720' \
-#  -e MAX_WORKERS=4 \
-#  -p 5014:5000 \
-#  --gpus all \
-#  gooey-gpu
+docker build . -t $NAME
 
-#NAME=sd-multi
-#docker rm -f $NAME
-#docker run -d --restart always \
-#  --name $NAME \
-#  -v $PWD/checkpoints:/src/checkpoints \
-#  -v $HOME/.cache/huggingface:/root/.cache/huggingface \
-#  -v $HOME/.cache/torch:/root/.cache/torch \
-#  -e SENTRY_DSN='https://e388de9646144cd9ae13f96a631b1707@o425905.ingest.sentry.io/4504612832542720' \
-#  -e MAX_WORKERS=1 \
-#  -p 5012:5000 \
-#  --gpus all \
-#  gooey-gpu
-
-
-NAME=lavis
 docker rm -f $NAME
 docker run -d --restart always \
   --name $NAME \
   -v $PWD/checkpoints:/src/checkpoints \
   -v $HOME/.cache/huggingface:/root/.cache/huggingface \
   -v $HOME/.cache/torch:/root/.cache/torch \
-  -e SENTRY_DSN='https://e388de9646144cd9ae13f96a631b1707@o425905.ingest.sentry.io/4504612832542720' \
   -e MAX_WORKERS=1 \
-  -p 5015:5000 \
+  -p 5012:5000 \
   --gpus all \
-  gooey-gpu
+  $NAME
 
 docker logs -f $NAME
